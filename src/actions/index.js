f6f01c84d8bf800b64021c03855c08ce97bed17f
MohammadOwais10 <owais0945@gmail.com>
2022-02-12 23:58:39 +0530

    Action creators

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import { data } from "../data";
+
+//action types
+export const ADD_MOVIES = "ADD_MOVIES";
+
+//action creators
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
+}
