2967ea0f3003c29d797440215786f7e5eaad0b52
MohammadOwais10 <owais0945@gmail.com>
2022-02-12 12:18:35 +0530

    Creating the Store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from "redux";
+import movies from "./reducers";
+
+const store = createStore(movies);
+console.log("store", store);
+console.log("STATE", store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
